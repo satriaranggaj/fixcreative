35a28c4c318529aad117e2544aab8049c6258b70
satriaranggaj <satriarangga43@gmail.com>
2025-05-24 02:28:13 +0700

    perbaikkan kecil

--- resources/js/Layouts/HomeLayout.vue
+++ resources/js/Layouts/HomeLayout.vue
@@ -3,9 +3,29 @@ import AppStore from '@/stores/ApplicationStore.js';
 import Navbar from '@/Components/Home/Organism/Navbar.vue';
 import AppMessageListener from '@/Components/Home/Organism/AppMessageListener.vue';
 import Footer from '@/Components/Home/Organism/Footer.vue';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+    <Head>
+        <title>Fix Creative - Jasa Multimedia & Event Organizer</title>
+        <meta name="description" content="Fix Creative Production House adalah penyedia layanan multimedia dan pengelola event profesional yang menghadirkan solusi kreatif untuk berbagai kebutuhan bisnis dan individu.">
+        <meta name="keywords" content="jasa fotografi, event organizer, wedding organizer, jasa prewedding, jasa foto produk, pembuatan website, production house, fix creative, fixcreative, wedding, event, studio, fotografer, videografer">
+        <meta name="author" content="Fix Creative">
+
+        <!-- Open Graph (Facebook & WhatsApp) -->
+        <meta property="og:title" content="Fix Creative - Jasa Multimedia & Event Organizer">
+        <meta property="og:description" content="Fix Creative hadir untuk kebutuhan fotografi, website, dan event Anda.">
+        <meta property="og:image" content="/img/logo/logo_fixcreative.jpg">
+        <meta property="og:url" :content="$page.url">
+
+        <!-- Twitter Card -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Fix Creative - Jasa Multimedia & Event Organizer">
+        <meta name="twitter:description" content="Fix Creative hadir untuk kebutuhan fotografi, website, dan event Anda.">
+        <meta name="twitter:image" content="/img/logo/logo_fixcreative.jpg">
+    </Head>
+
     <n-config-provider 
         :theme="AppStore.theme" 
         :theme-overrides="AppStore.themeOverrides" 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,77 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        @php
-            $isArticlePage = request()->is('home/*') && isset($article);
-            $isPortfolioPage = request()->is('portfolio/*') && isset($portfolio);
-        @endphp
-
-        <meta name="description" content="
-            {{ 
-                $isArticlePage 
-                    ? ($article['excerpt'] ?? $article['title']) 
-                    : ($isPortfolioPage 
-                        ? ($portfolio['excerpt'] ?? $portfolio['title']) 
-                        : 'Fix Creative Production House adalah penyedia layanan multimedia dan pengelola event profesional yang menghadirkan solusi kreatif untuk berbagai kebutuhan bisnis dan individu.'
-                    )
-            }}">
-        <meta property="og:title" content="
-            {{ 
-                $isArticlePage 
-                    ? $article['title'] 
-                    : ($isPortfolioPage 
-                        ? $portfolio['title'] 
-                        : 'Fix Creative - Jasa Multimedia & Event Organizer'
-                    )
-            }}">
-        <meta property="og:description" content="
-            {{ 
-                $isArticlePage 
-                    ? ($article['excerpt'] ?? $article['title']) 
-                    : ($isPortfolioPage 
-                        ? ($portfolio['excerpt'] ?? $portfolio['title']) 
-                        : 'Fix Creative hadir untuk kebutuhan fotografi, website, dan event Anda.'
-                    )
-            }}">
-        <meta property="og:image" content="
-            {{ 
-                $isArticlePage 
-                    ? asset('storage/' . $article['image']) 
-                    : ($isPortfolioPage 
-                        ? asset('storage/' . $portfolio['image']) 
-                        : asset('img/logo/logo_fixcreative.jpg')
-                    )
-            }}">
-        <meta property="og:url" content="{{ url()->current() }}">
-
-        <meta name="twitter:title" content="
-            {{ 
-                $isArticlePage 
-                    ? $article['title'] 
-                    : ($isPortfolioPage 
-                        ? $portfolio['title'] 
-                        : 'Fix Creative - Jasa Multimedia & Event Organizer'
-                    )
-            }}">
-        <meta name="twitter:description" content="
-            {{ 
-                $isArticlePage 
-                    ? ($article['excerpt'] ?? $article['title']) 
-                    : ($isPortfolioPage 
-                        ? ($portfolio['excerpt'] ?? $portfolio['title']) 
-                        : 'Fix Creative hadir untuk kebutuhan fotografi, website, dan event Anda.'
-                    )
-            }}">
-        <meta name="twitter:image" content="
-            {{ 
-                $isArticlePage 
-                    ? asset('storage/' . $article['image']) 
-                    : ($isPortfolioPage 
-                        ? asset('storage/' . $portfolio['image']) 
-                        : asset('img/logo/logo_fixcreative.jpg')
-                    )
-            }}">
-
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" href="{{ asset('img/logo/logo_fixcreative.ico') }}" type="image/x-icon">
 
