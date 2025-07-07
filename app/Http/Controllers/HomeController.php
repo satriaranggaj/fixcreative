0536f4227633ae655d2b0d4eeba899765781ef3c
satriaranggaj <satriarangga43@gmail.com>
2025-07-07 23:53:08 +0700

    add meta tag in division

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
                 'title' => 'Studio – Brand Strategy, Visual Communication, Photography & Videography Services',
                 'description' => 'Divisi Studio dari Fix Creative Production House menyediakan layanan yang menggabungkan strategi brand, desain visual, serta fotografi dan videografi profesional untuk berbagai kebutuhan. Kami percaya bahwa tampilan visual yang kuat dan relevan mampu meningkatkan daya tarik brand dan memperkuat koneksi dengan audiens secara emosional.',
                 'keywords' => 'fotografer, videografer, studio fotografi, studio videografi, studio fotografi bekasi, studio videografi bekasi',
-                'image' => asset('img/division/studio.webp'),
+                'image' => asset('img/division/studio.jpg'),
             ],
         ]);
     }
@@ -27,7 +27,7 @@ class HomeController extends Controller
                 'title' => 'Agency – Multimedia Content Production, Branding & Digital Marketing',
                 'description' => 'Divisi Agency dari Fix Creative Production House berfokus pada produksi konten multimedia dan pengembangan branding melalui strategi digital marketing yang terukur. Kami hadir untuk memenuhi kebutuhan komunikasi visual yang relevan dengan zaman digital, membantu bisnis tumbuh melalui konten yang beresonansi dengan audiensnya.',
                 'keywords' => 'agency, jasa multimedia, jasa personal branding, digital marketing, personal branding',
-                'image' => asset('img/division/agency.webp'),
+                'image' => asset('img/division/agency.jpg'),
             ]
         ]);
     }
@@ -38,7 +38,7 @@ class HomeController extends Controller
                 'title' => 'Fotografi – Wedding & Event Documentation',
                 'description' => 'Divisi Fotografi dari Fix Creative Production House berfokus pada dokumentasi acara dan pernikahan, menangkap setiap momen dengan gaya visual storytelling yang kuat. Kami tidak hanya sekadar mengambil gambar, tetapi menghadirkan sebuah cerita melalui setiap foto dan video.',
                 'keywords' => 'fotografer, fotografi, fotografi wedding, fotografi event, fotografi wedding bekasi, fotografi event bekasi',
-                'image' => asset('img/division/photography.webp'),
+                'image' => asset('img/division/photography.jpg'),
             ]
         ]);
     }
