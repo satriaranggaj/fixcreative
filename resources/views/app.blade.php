f5d43b3f84ecf1146802a9911555013f64eabfec
satriaranggaj <satriarangga43@gmail.com>
2025-07-16 09:48:05 +0700

    fix seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,20 +25,28 @@
     <meta name="twitter:description" content="{{ $meta['description'] ?? 'Fix Creative hadir untuk kebutuhan fotografi, website, dan event Anda.' }}">
     <meta name="twitter:image" content="{{ $meta['image'] ?? asset('img/logo/logo_fixcreative.jpg') }}">
 
-    <!-- Structured Data for Logo -->
-    <script type="application/ld+json">
-    {
-    "@context": "https://schema.org",
-    "@type": "Organization",
-    "name": "Fix Creative",
-    "url": "https://fixcreative.co.id",
-    "logo": "https://fixcreative.co.id/img/logo/logo_fixcreative.jpg"
-    }
-    </script>
-
     <title inertia>{{ $meta['title'] ?? config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="{{ asset('img/logo/logo_fixcreative.ico') }}" type="image/x-icon">
+    <meta name="msapplication-config" content="{{ asset('favicons/browserconfig.xml') }}">
+    <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicons/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/manifest.json') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('favicons/ms-icon-144x144.png') }}">
+    <meta name="theme-color" content="#ffffff">
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="canonical" href="{{ url()->current() }}" />
 
